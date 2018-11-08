6deb1d9f292418be605600a662da38792660d089
Artem <freecats1997@gmail.com>
2018-11-08 17:55:33 +0600

    Fix Bug

--- Lesson2/task5.rb
+++ Lesson2/task5.rb
@@ -1,7 +1,7 @@
 DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def year_is_vis?(year)
-  (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+  (year % 4).zero? && (year % 100 != 0) || (year % 400).zero?
 end
 
 puts "Введите число"
@@ -12,5 +12,6 @@ puts "Введите год"
 year = gets.chomp.to_i
 
 DAYS_IN_MONTH[2] = 29 if year_is_vis?(year)
+day_number = DAYS_IN_MONTH[1..month - 1].sum + day
 
-puts "#{day}.#{month}.#{year} это #{DAYS_IN_MONTH[1..month - 1].sum} день с начала года"
+puts "#{day}.#{month}.#{year} это #{day_number} день с начала года"
