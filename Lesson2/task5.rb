a7900a8aae6b368b79387f3c449ce3d461f61778
Artem <freecats1997@gmail.com>
2018-11-08 13:13:32 +0600

    Lesson 2 v 1.1

--- Lesson2/task5.rb
+++ Lesson2/task5.rb
@@ -1,19 +1,7 @@
 DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def year_is_vis?(year)
-  year_is_vis = false
-  if (year % 4 == 0) && (year % 100 != 0)
-    year_is_vis = true
-  elsif year % 400 == 0
-    year_is_vis = true
-  end
-
-  year_is_vis
-end
-
-def days_in_month(month, year)
-  return 29 if month == 2 && year_is_vis?(year)
-  DAYS_IN_MONTH[month]
+  (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
 end
 
 puts "Введите число"
@@ -23,16 +11,6 @@ month = gets.chomp.to_i
 puts "Введите год"
 year = gets.chomp.to_i
 
-result_count = 0
-(1..month - 1).each do |month_in_range|
-  result_count += days_in_month(month_in_range, year)
-end
-result_count += day
+DAYS_IN_MONTH[2] = 29 if year_is_vis?(year)
 
-puts "#{day}.#{month}.#{year} это #{result_count} день с начала года"
-
-
-
-
-
-
+puts "#{day}.#{month}.#{year} это #{DAYS_IN_MONTH[1..month - 1].sum} день с начала года"
--- Lesson2/task6.rb
+++ Lesson2/task6.rb
@@ -2,30 +2,21 @@ purchases = {}
 all_total = 0
 
 loop do
-  puts "Введите название товара"
+  puts 'Введите название товара'
   name = gets.chomp
   break if name == 'стоп'
 
-  puts "Введите цену товара"
+  puts 'Введите цену товара'
   cost = gets.chomp.to_f
-  puts "Введите количество товара"
+  puts 'Введите количество товара'
   quantity = gets.chomp.to_i
 
-  purchases[name] = {cost: cost, quantity: quantity, total: cost * quantity}
-
-  # Тут если не храним в хеше
-  # purchases[name] = {cost: cost, quantity: quantity}
+  purchases[name] = { cost: cost, quantity: quantity, total: cost * quantity }
 end
 
 purchases.each_with_index do |(key, value), index|
-  total_row = value[:total]
-  puts "#{index + 1}: #{key.to_s}, количество: #{value[:quantity]}, цена: #{value[:cost]}, сумма: #{total_row}"
-
-  # Тут если не храним в хеше
-  # total_row = value[:quantity] * value[:cost]
-  # puts "#{index}: #{key.to_s}, количество: #{value[:quantity]}, цена: #{value[:cost]}, сумма: #{value[:quantity] * value[:cost]}"
-
-  all_total += total_row
+  puts "#{index + 1}: #{key}, количество: #{value[:quantity]}, цена: #{value[:cost]}, сумма: #{value[:total]}"
+  all_total += value[:total]
 end
 
 puts "Всего покупок на сумму #{all_total}"
